1d90ad8d346f646290708349b4191bd0d45dabe2
GregoryUp <grisha.terteryan@mail.ru>
2021-12-19 15:06:13 +0300

    Multiusers Realesed

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const TelegrammBotApi = require('node-telegram-bot-api');
 const mysql = require('mysql');
 const fs = require('fs');
 
-const TOKEN = '5027183755:AAHNfjLxSMQ2UnhHjDJ6rA6cXuq33BAWZIE';
-
-const bot = new TelegrammBotApi(TOKEN, {
-    polling: true
-})
+const TOKEN = '5038783257:AAGLlQGQiWN036D-B-SrRjaWsaQ0o7Xs-RE';
 
 var connection = mysql.createConnection({
     host: '127.0.0.1',
@@ -15,8 +11,6 @@ var connection = mysql.createConnection({
     password: '',
     database: 'students'
 });
-
-var step = 'begin';
 
 // var arr_smiles = [
 //     ['\ud83d\ude05', '\ud83d\ude18', '\ud83d\ude09'],
@@ -42,132 +36,166 @@ var arr_days_assoc = {
     'СБ': 'SAT'
 }
 
-var timetable_button = [
+class MyBotFunction {
 
-]
+    step = 'begin'
 
-bot.on('text', msg => {
-    const chat_id = msg.chat.id;
-    const { text } = msg;
+    bot = new TelegrammBotApi(TOKEN, {
+        polling: true
+    })
 
-    if (/^\/start/.test(text.trim()) || step == 'begin') {
+    constructor() {
 
-        bot.sendMessage(chat_id, `Здравствуйте, введите номер зачётки!`, {
-            reply_markup: {
-                remove_keyboard: true
-            }
-        })
-            .then(() => {
-                step = 'zachetka';
-            });
-        return;
     }
 
-    switch (step) {
-        case 'zachetka':
-            connection.query(
-                `SELECT * FROM students WHERE doc_number = '${text}'`,
-                function (error, results, fields) {
-                    if (error) {
-                        console.log(error);
+    start() {
+        const newBot = this.bot;
+        var new_step = this.step;
+        newBot.on('text', msg => {
+            const chat_id = msg.chat.id;
+            const { text } = msg;
 
-                        bot.sendMessage(chat_id, `Номер зачётки не найден в базе!`, {
-                            reply_markup: {
-                                remove_keyboard: true
-                            }
-                        })
-                            .then(() => {
-                                step = 'zachetka';
-                            });
-                    } else {
-                        console.log(results);
-                        if (results.length == 0)
-                            bot.sendMessage(chat_id, `Номер зачётки не найден в базе!`, {
-                                reply_markup: {
-                                    remove_keyboard: true
-                                }
-                            })
-                                .then(() => {
-                                    step = 'zachetka';
-                                });
-                        else
-                            bot.sendMessage(chat_id, `Здравствуйте ${results[0].fio}!`, {
-                                reply_markup: {
-                                    keyboard: choice_begin_button
-                                }
-                            })
-                                .then(() => {
-                                    step = 'timetable';
-                                });
-                    }
-                }
-            );
+            if (/^\/start/.test(text.trim()) || new_step == 'begin') {
 
-            break;
-        case 'timetable':
-            if (text == 'Узнать расписание') {
-                bot.sendMessage(chat_id, `Выберите день недели!`, {
-                    reply_markup: {
-                        keyboard: choice_days_button
-                    }
-                }).then(() => {
-                    step = 'timetable_day'
-                }).catch((err) => console.log(err));
-
-            } else if (text == 'Выход') {
-                bot.sendMessage(chat_id, `Вы вышли`)
-                    .then(() => {
-                        step = 'zachetka';
-                    })
-                bot.sendMessage(chat_id, `Здравствуйте, введите номер зачётки!`, {
+                newBot.sendMessage(chat_id, `Здравствуйте, введите номер зачётки!`, {
                     reply_markup: {
                         remove_keyboard: true
                     }
                 })
-            }
-            break;
-        case 'timetable_day':
-            let tableString = fs.readFileSync(path.join(__dirname, 'timetable.json'));
-            let table = JSON.parse(tableString);
-
-            if (arr_days_assoc.hasOwnProperty(text)) {
-                let lessons_data = table[arr_days_assoc[text]].lessons;
-                let str = '';
-
-                for (let i = 0; i < lessons_data.length; i++) {
-                    str += (i + 1) + " Пара " + " - " + lessons_data[i].item + ";" + "\n" +
-                        "Время" + " " + lessons_data[i].time + "\n" +
-                        "№ Кабинета " + lessons_data[i].room + "\n" +
-                        "Преподаватель" + " " + lessons_data[i].prepod + "\n" + "\n";
-                }
-
-                bot.sendMessage(chat_id, "Ваше расписание: \n" + str);
-            } else if ( text == 'Вся неделя' ) {
-                let str = ''; 
-
-                for(let i = 0; i < choice_days_button[0].length; i++){
-
-                    str += choice_days_button[0][i] + "\n\n";
-
-                    for (let y = 0; y < table[arr_days_assoc[choice_days_button[0][i]]].lessons.length; y++){
-                        str += (y + 1) + " Пара " + " - " + table[arr_days_assoc[choice_days_button[0][i]]].lessons[y].item + ";" + "\n" +
-                        "Время" + " " + table[arr_days_assoc[choice_days_button[0][i]]].lessons[y].time + "\n" +
-                        "№ Кабинета " + table[arr_days_assoc[choice_days_button[0][i]]].lessons[y].room + "\n" +
-                        "Преподаватель" + " " + table[arr_days_assoc[choice_days_button[0][i]]].lessons[y].prepod + "\n" + "\n";
-                    }
-                    
-                }
-
-                bot.sendMessage(chat_id, "Ваше расписание: \n" + str);
-            } else if ( text == 'Вернуться' ) {
-                bot.sendMessage(chat_id, `Введите номер зачётки!`, {
-                    reply_markup:{remove_keyboard: true}
-                })
-                    .then(()=>step="zachetka")
+                    .then(() => {
+                        new_step = 'zachetka';
+                    });
+                return;
             }
 
-            break;
+
+            switch (new_step) {
+                case 'zachetka':
+                    connection.query(
+                        `SELECT * FROM students WHERE doc_number = '${text}'`,
+                        function (error, results, fields) {
+                            if (error) {
+                                console.log(error);
+
+                                newBot.sendMessage(chat_id, `Номер зачётки не найден в базе!`, {
+                                    reply_markup: {
+                                        remove_keyboard: true
+                                    }
+                                })
+                                    .then(() => {
+                                        new_step = 'zachetka';
+
+
+                                    });
+                            } else {
+                                console.log(results);
+                                if (results.length == 0)
+                                    newBot.sendMessage(chat_id, `Номер зачётки не найден в базе!`, {
+                                        reply_markup: {
+                                            remove_keyboard: true
+                                        }
+                                    })
+                                        .then(() => {
+                                            new_step = 'zachetka';
+                                        });
+                                else
+                                    newBot.sendMessage(chat_id, `Здравствуйте ${results[0].fio}!`, {
+                                        reply_markup: {
+                                            keyboard: choice_begin_button
+                                        }
+                                    })
+                                        .then(() => {
+                                            let user_step = 'timetable';
+                                            new_step = 'registered';
+                                            connection.query(`SELECT * FROM user WHERE chat_id = '${chat_id}'`, (err, results) => {
+                                                if (results.length == 0) {
+                                                    connection.query(`INSERT INTO user (id, chat_id, user_step) VALUES (NULL, '${chat_id}', '${user_step}')`)
+                                                } else {
+                                                    connection.query(`UPDATE user SET user_step = '${user_step}' WHERE chat_id = '${chat_id}';`)
+                                                }
+                                            })
+
+                                        });
+                            }
+                        }
+                    );
+                    break;
+                case 'registered':
+
+                    connection.query(`SELECT * FROM user WHERE chat_id = '${chat_id}'`, (error, results) => {
+                        var user_step = results[0].user_step;
+                        console.log(user_step);
+                        if (user_step == 'timetable') {
+                            if (text == 'Узнать расписание') {
+                                newBot.sendMessage(chat_id, `Выберите день недели!`, {
+                                    reply_markup: {
+                                        keyboard: choice_days_button
+                                    }
+                                }).then(() => {
+                                    user_step = 'timetable_day';
+                                    connection.query(`UPDATE user SET user_step = '${user_step}' WHERE chat_id = '${chat_id}'`)
+                                }).catch((err) => console.log(err));
+
+                            } else if (text == 'Выход') {
+                                newBot.sendMessage(chat_id, `Вы вышли`)
+                                    .then(() => {
+                                        new_step = 'zachetka';
+                                    })
+                                newBot.sendMessage(chat_id, `Здравствуйте, введите номер зачётки!`, {
+                                    reply_markup: {
+                                        remove_keyboard: true
+                                    }
+                                })
+                            }
+                        } else if (user_step == 'timetable_day') {
+                            let tableString = fs.readFileSync(path.join(__dirname, 'timetable.json'));
+                            let table = JSON.parse(tableString);
+
+                            if (arr_days_assoc.hasOwnProperty(text)) {
+                                let lessons_data = table[arr_days_assoc[text]].lessons;
+                                let str = '';
+
+                                for (let i = 0; i < lessons_data.length; i++) {
+                                    str += (i + 1) + " Пара " + " - " + lessons_data[i].item + ";" + "\n" +
+                                        "Время" + " " + lessons_data[i].time + "\n" +
+                                        "№ Кабинета " + lessons_data[i].room + "\n" +
+                                        "Преподаватель" + " " + lessons_data[i].prepod + "\n" + "\n";
+                                }
+
+                                newBot.sendMessage(chat_id, "Ваше расписание: \n" + str);
+                            } else if (text == 'Вся неделя') {
+                                let str = '';
+
+                                for (let i = 0; i < choice_days_button[0].length; i++) {
+
+                                    str += choice_days_button[0][i] + "\n\n";
+
+                                    for (let y = 0; y < table[arr_days_assoc[choice_days_button[0][i]]].lessons.length; y++) {
+                                        str += (y + 1) + " Пара " + " - " + table[arr_days_assoc[choice_days_button[0][i]]].lessons[y].item + ";" + "\n" +
+                                            "Время" + " " + table[arr_days_assoc[choice_days_button[0][i]]].lessons[y].time + "\n" +
+                                            "№ Кабинета " + table[arr_days_assoc[choice_days_button[0][i]]].lessons[y].room + "\n" +
+                                            "Преподаватель" + " " + table[arr_days_assoc[choice_days_button[0][i]]].lessons[y].prepod + "\n" + "\n";
+                                    }
+
+                                }
+
+                                newBot.sendMessage(chat_id, "Ваше расписание: \n" + str);
+                            } else if (text == 'Вернуться') {
+                                newBot.sendMessage(chat_id, `Введите номер зачётки!`, {
+                                    reply_markup: { remove_keyboard: true }
+                                })
+                                    .then(() => new_step = "zachetka")
+                            }
+                        }
+
+                    });
+
+                    break;
+            }
+
+        });
     }
 
-});
+}
 
+new MyBotFunction().start();
